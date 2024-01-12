02aa3f8cdc12af0d5c0b10ff171c3f940da90de9
Gustavo Teixeira <gustavohteixeira2904@gmail.com>
2024-01-12 11:40:08 -0300

    .

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.Random;
 /**
@@ -13,6 +14,9 @@ public class App
         System.out.println( "Hello World!" );
         int[][] arr = make(10,10);
         printArr(arr);
+        int[][] next = make(10,10);
+        System.out.println();
+        printArr(next);
     }
 
     public static int [][] make(int cols, int rows){
@@ -30,7 +34,34 @@ public class App
             System.out.println(Arrays.toString(arr[i]));
         }
     }
-    public static void life(int[][] arr){
+    public static void life(int [][] arr){
+        int[][] nextArr = make(10,10);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                int count = countNeighbors(arr);
+//                int sum = 0;
+//                sum+= nextArr [i-1][j - 1];
+//                sum+= nextArr [i-1][j];
+//                sum+= nextArr [i-1][j + 1];
+//                sum+= nextArr [i][j - 1];
+//                sum+= nextArr [i][j + 1];
+//                sum+= nextArr [i+1][j - 1];
+//                sum+= nextArr [i+1][j];
+//                sum+= nextArr [i+1][j + 1];
 
+
+            }
+        }        
+
+    }
+    public static int countNeighbors(int[][] arr, int x,int y){
+        int sum = 0;
+        for (int i = 0;i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                sum += arr [i][j];
+            }
+        }
+        sum -= arr[x][y];
+        return sum;
     }
 }
